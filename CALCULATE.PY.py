1d71c3827d55b13064be0e1b925cf026d10fa708
HARSH MISHRA <hk.mishra521@gmail.com>
2023-01-09 23:08:45 +0530

    Add files via upload

---
+++ CALCULATE.PY.py
@@ -0,0 +1,7 @@
+print("CALCULATOR")
+print("enter the number ")
+a=input()
+print("enetr the number")
+b =input()
+print("the sum is ",int(a)+int(b))
+
---
+++ DICTION.py
@@ -0,0 +1,22 @@
+
+d={"python":"a easiest programming language","c":"a old programming language","java":"most used programming language","sql":"a database language"}
+print(d)
+print("enter the word :")
+w=input()
+print(d[w])
+
+info = {'name':'Karan', 'age':19, 'eligible':True}
+print(info.items())
+
+info = {'name':'Karan', 'age':19, 'eligible':True}
+print(info.keys())
+
+info = {'name':'Karan', 'age':19, 'eligible':True}
+print(info.values())
+
+info = {'name':'Karan', 'age':19, 'eligible':True}
+print(info['name'])
+print(info.get('eligible'))
+
+info = {'name':'Karan', 'age':19, 'eligible':True}
+print(info)

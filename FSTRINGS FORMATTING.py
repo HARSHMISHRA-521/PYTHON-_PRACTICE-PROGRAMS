11943211320a8f641cccf6af44d4c109212e7b34
HARSH MISHRA <hk.mishra521@gmail.com>
2023-01-15 23:05:09 +0530

    COMMIT

---
+++ FSTRINGS FORMATTING.py
@@ -0,0 +1,15 @@
+txt = "For only {price:.2f} dollars!"    #old method of string formatting
+print(txt.format(price = 49))
+letter ="MY NAME IS {} AND I AM {} YEARS OLD"
+name="HARSH"
+age=22
+print(letter.format(name,age))
+
+val = 'Geeks'                     #new method of f-strings
+print(f"{val}for{val} is a portal for {val}.")
+name = 'Tushar'
+age = 23
+
+print(f"Hello, My name is {name} and I'm {age} years old.")
+
+print(f"{2 * 30})")

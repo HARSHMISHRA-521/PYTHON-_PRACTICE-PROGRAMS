7966046cbbf45b7a018ab616db474f0560d6122f
HARSH MISHRA <hk.mishra521@gmail.com>
2023-02-06 23:03:59 +0530

    first commit

---
+++ import_file.py
@@ -0,0 +1,55 @@
+
+
+# from math import sqrt, pi
+# from math import pi, sqrt as s
+# import math as math_builtin_python
+
+# result = math_builtin_python.sqrt(9) * math_builtin_python.pi
+# print(result)  # Output: 3.0
+
+# from harry import welcome, harry
+import sets as hr
+import math
+
+print(dir(math))
+print(math.nan, type(math.nan))
+hr.welcome()
+print(hr.harry)
+
+import math
+import math
+
+result = math.sqrt(9)
+print(result)  # Output: 3.0
+
+from math import sqrt
+
+result = sqrt(9)
+print(result)  # Output: 3.0
+
+from math import sqrt, pi
+
+result = sqrt(9)
+print(result)  # Output: 3.0
+
+print(pi)  # Output: 3.141592653589793
+
+from math import *
+#It's also possible to import all functions and variables from a module using the * wildcard.
+# However, this is generally not recommended as it can lead to confusion and make it
+# harder to understand where specific functions and variables are coming from.
+result = sqrt(9)
+print(result)  # Output: 3.0
+
+print(pi)  # Output: 3.141592653589793
+
+import math as m
+
+result = m.sqrt(9)
+print(result)  # Output: 3.0
+
+print(m.pi)  # Output: 3.141592653589793
+
+import math
+
+print(dir(math))

f017af6a3fa7a2d3a056e14bed645423090e4761
HARSH <mishraharshmishra2020@gmail.com>
2023-04-15 18:27:45 +0530

    first commit

---
+++ operator overriding.py
@@ -0,0 +1,28 @@
+class Vector:
+  def __init__(self, i, j, k):
+    self.i = i
+    self.j = j
+    self.k = k
+
+  def __str__(self):
+    return f"{self.i}i + {self.j}j + {self.k}k"
+
+  def __add__(self, x):
+    return Vector(self.i + x.i,  self.j+x.j, self.k+x.k)
+v1 = Vector(3, 5, 6)
+print(v1)
+
+v2 = Vector(1, 2, 9)
+print(v2)
+
+print(v1 + v2)
+print(type(v1 + v2))
+
+#
+# + : __add__
+# - : __sub__
+# * : __mul__
+# / : __truediv__
+# < : __lt__
+# > : __gt__
+# == : __eq__

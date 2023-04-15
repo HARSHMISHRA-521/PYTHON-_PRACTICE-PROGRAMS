a84c41c5c1a8e6ed2b8c60f0348b2d6fb4cfdf46
HARSH <mishraharshmishra2020@gmail.com>
2023-04-16 02:28:05 +0530

    first commit

---
+++ Multiple_Inheritance.py
@@ -0,0 +1,26 @@
+class Employee:
+  def __init__(self, name):
+    self.name = name
+  def show(self):
+    print(f"The name is {self.name}")
+
+class Dancer:
+  def __init__(self, dance):
+    self.dance = dance
+
+  def show(self):
+    print(f"The dance is {self.dance}")
+
+class DancerEmployee(Employee, Dancer):
+  def __init__(self, dance, name):
+    # self.dance = dance
+    # self.name = name
+    #        or
+    Employee.__init__(self,name)
+    Dancer.__init__(self,dance)
+
+o  = DancerEmployee("Kathak", "Shivani")
+print(o.name)
+print(o.dance)
+o.show()
+print(DancerEmployee.mro())
